5c0c5ea26458837623b4a3839468fb2a72b89a7b
Simphiwe Tebe <simphiwetebe@gmail.com>
2021-04-20 20:37:50 +0200

    Completed reducers for shopping cart

--- src/redux/Shopping/shopping-reducer.js
+++ src/redux/Shopping/shopping-reducer.js
@@ -74,23 +74,32 @@ const INITIAL_STATE = {
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
         case actionTypes.ADD_TO_CART:
-            return {
+            // get item data from products and check if already in cart
+            const cartItem = state.products.find(prod => prod.id === action.payload.id)
+            const inCart = state.cart.find(item => item.id === action.payload.id ? true : false)
 
+            return {
+                ...state,
+                cart: inCart ? state.cart.map(item => item.id === action.payload.id ? {...item, qty: item.qty + 1} : item)
+                : [...state.cart, {...cartItem, qty: 1}]
             }
 
         case actionTypes.REMOVE_FROM_CART:
             return {
-                
+                ...state,
+                cart: state.cart.filter(item => item.id !== action.payload.id)
             }
 
         case actionTypes.ADJUST_QTY:
             return {
-                
+                ...state,
+                cart: state.cart.map(item => item.id === action.payload.id ? {...item, qty: action.payload.qty} : item)
             }
 
         case actionTypes.LOAD_CURRENT_ITEM:
             return {
-                
+                ...state,
+                currentItem: action.payload
             }
 
         default:
